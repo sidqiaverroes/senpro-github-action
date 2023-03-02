e7e2fcb2fdac2f2f8b2439e6ee9f21d43615ba3b
sidqiaverroes <sidqi.averroes26@gmail.com>
2023-03-02 09:17:38 +0700

    feat: add case 3-test build gagal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import { Inter } from '@next/font/google'
+// import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            [Nama] | [NIU]
+            Sidqi Averroes | 20/460568/TK/51157
           </h5>
         </div>
       </main>
